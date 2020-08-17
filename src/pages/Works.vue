3258b947798afe01703edf3227f5c3b8cfd00385
鍾元榮 <herbtea211@gmail.com>
2020-08-17 17:01:11 +0800

    不用slot

--- src/pages/Works.vue
+++ src/pages/Works.vue
@@ -1,29 +1,32 @@
 <template lang="pug">
   #works
     h1 works
-    router-view
+    div(:is="page")
 </template>
 
 <script>
 
+import griphic01 from '@/components/works_compoments/griphic/01'
+
+
 export default {
   name: 'worksPage',
     components: {
-   
+        griphic01
     },
     data () {
     return {
-      
+      page: null
     }
   },
   computed: {
     
   },
   mounted () {
-    
+    this.page = griphic01
   },
   methods: {
-
+      
   },
   watch: {
 
