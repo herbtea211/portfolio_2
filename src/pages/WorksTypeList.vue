30f2a5e51ad72595e0287b7c7ebdb7191d5861d3
鍾元榮 <herbtea211@gmail.com>
2020-08-19 14:00:41 +0800

    測試成功

--- src/pages/WorksTypeList.vue
+++ src/pages/WorksTypeList.vue
@@ -1,14 +1,18 @@
 <template lang="pug">
   #work-type-list
     h1 workTypeList
+    .test
+      h1 500
+    sidebarCompoment
 </template>
 
 <script>
+import sidebarCompoment from '@/components/SideBarComponent'
 
 export default {
   name: 'workTypeListPage',
     components: {
-   
+      sidebarCompoment
     },
     data () {
     return {
@@ -32,6 +36,21 @@ export default {
 
 <style lang="sass">
 
+@keyframes testAnimation
+  from
+    background-color: hsl(0, 0, 0)
+  to
+    background-color: hsl(220, 20, 60)
+
 #work-type-list
+  .test
+    width: 500px
+    height: 500px
+    background-color: hsl(0, 0, 0)
+    -webkit-background-clip: text
+    background-clip: text
+    color: transparent
+    animation: testAnimation 5s
+    animation-iteration-count: infinite
   
 </style>
