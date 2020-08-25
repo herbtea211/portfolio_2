e110a8793ed0809e6edfe5a604eff2a1cfd5a485
鍾元榮 <herbtea211@gmail.com>
2020-08-25 09:07:44 +0800

    換一下圖

--- src/components/works_compoments/ui/MovieStore.vue
+++ src/components/works_compoments/ui/MovieStore.vue
@@ -16,29 +16,42 @@
         lineColor = "#2bdeb6"
       )
       p 所以最重要的點只有一個,訂位流程圖像化~~用區塊的方式把位置先劃分出來,再從區塊裡選位置,至於區塊如何劃分是在後台設定,這部分就先省略
-    .view-box(
-      v-for="(item, index) in copyWritingConfig"
-      :key="index"
-    )
+    .view-box
       .content-box
         .img-box
           img(
-            :src="require(`@/assets/images/works_img/ui/movie_store/${item.src}.png`)"
+            :src="require('@/assets/images/works_img/ui/movie_store/1.png')"
           )
         .text-box
-          h2 {{item.title}}
+          h2 訂位前準備
           .p-box
-            p {{item.copyWriting}}
+            p 這裡先把除了定位以外的流程走完,也可以說這裡一定要先做完才有辦法選位置
             .line
             .BG-Block
-    //- .view-box
-    //-   img(
-    //-     :src="require('@/assets/images/works_img/ui/movie_store/2.png')"
-    //-   )
-    //- .view-box
-    //-   img(
-    //-     :src="require('@/assets/images/works_img/ui/movie_store/3.png')"
-    //-   )
+    .view-box.bigger.center
+      .content-box
+        .img-box
+          img(
+            :src="require('@/assets/images/works_img/ui/movie_store/2.png')"
+          )
+        .text-box
+          h2 先選擇區塊
+          .p-box
+            p 這裡把所有位置先用區塊分開,可以很清楚知道位置靠那,還有整個廳院的動線,當然每個廳會有不一樣的設定,點擊之後可顯示目前剩幾位,一大群人要去,或者一位難求的電影這時候就很有用,確認完之後再點擊下一步
+            .line
+            .BG-Block
+    .view-box.bigger.bottom
+      .content-box
+        .img-box
+          img(
+            :src="require('@/assets/images/works_img/ui/movie_store/3.png')"
+          )
+        .text-box
+          h2 再選擇座位
+          .p-box
+            p 這裡就比較易懂了,用顏色區分可選的位置讓user點擊,如果人很多,可以在這裡先選一半,回上一頁找其他區塊的位置,當然劃分區塊之前就要先規劃好,一個區有幾排幾列,這裡先預計 10 x 10 ,再往下走就是確認訂單了
+            .line
+            .BG-Block
 </template>
 
 <script>
@@ -53,23 +66,7 @@ export default {
     },
     data () {
     return {
-      copyWritingConfig: [
-        {
-          src: '1',
-          title: '訂位前準備',
-          copyWriting: '這裡先把除了定位以外的流程走完,也可以說這裡一定要先做完才有辦法選位置'
-        },
-        {
-          src: '2',
-          title: '先選擇區塊',
-          copyWriting: '這裡把所有位置先用區塊分開,可以很清楚知道位置靠那,還有整個廳院的動線,當然每個廳會有不一樣的設定,點擊之後可顯示目前剩幾位,一大群人要去,或者一位難求的電影這時候就很有用,確認完之後再點擊下一步'
-        },
-        {
-          src: '3',
-          title: '再選擇座位',
-          copyWriting: '這裡就比較易懂了,用顏色區分可選的位置讓user點擊,如果人很多,可以在這裡先選一半,回上一頁找其他區塊的位置,當然劃分區塊之前就要先規劃好,一個區有幾排幾列,這裡先預計 10 x 10 ,再往下走就是確認訂單了'
-        }
-      ]
+
     }
   },
   computed: {
@@ -109,9 +106,6 @@ $focusColor: #2bdeb6
   .order-way
     background-color: #1d1d1d
     padding: 20px 0px 20px 0px
-    // p
-    //   @include pStyle(60%)
-    //   margin: 50px auto
   .view-box
     position: relative
     width: 100%
@@ -151,16 +145,56 @@ $focusColor: #2bdeb6
             position: absolute
             top: 0
             left: -70%
+  .view-box.bigger
+      .content-box
+        .img-box
+          width: 80%
+  .view-box.bigger.center
+    .content-box
+      .text-box
+        .p-box
+          .line
+            width: 140%
+          .BG-Block
+            height: 235px
+            left: -45%
+  .bottom
+    margin: 10vh auto 0vh
+    padding-bottom: 10vh
+    .content-box
+      .text-box
+        .p-box
+          .line
+            width: 200%
+          .BG-Block
+            height: 115px
+            left: -60%
+            width: 150%
+
+@media screen and (max-width: 1400px)
+  #movie-store
+    .view-box
+      .content-box
+        width: 60%
+        margin: 200px auto
+        display: flex
 
 @media screen and (max-width: 450px)
   @media (orientation: portrait) // 直視 , landscape 橫式
     #movie-store
+      p
+        width: 85%
       .top-flow-img-box
         img
           width: 80%
       .view-box
         .content-box
           width: 90%
+          margin: 150px auto
           .img-box
             width: 60%
+      .view-box.bigger
+        .content-box
+          .img-box
+            width: 165%
 </style>
