339d35c71e75ed33ecba32a2c88fa891e849a0a3
鍾元榮 <herbtea211@gmail.com>
2020-08-31 14:19:56 +0800

    add done

--- src/components/works_compoments/gripgic/W_M.vue
+++ src/components/works_compoments/gripgic/W_M.vue
@@ -1,6 +1,14 @@
 <template lang="pug">
   #w_m
-    h1 w_m
+    .left-side-box
+      img.logo(
+        :src="require('@/assets/images/works_img/griphic/w_m/0.png')"
+      )
+      h4 這是一間由 Webber 跟 Mark 兩個人組成的攝影工作室,主要業務是婚禮動靜態的拍攝,這兩位業主是公司同事,利用下班時間兼職攝影,其實兩個人的生活很緊密的連結在一起,剛好名字字首一個'M'一個‘W’很直覺的就是要利用這兩個字來發想,logo的字體實際上就是同一個字上下顛倒而且連在一起,我想應該也不用太多解釋,而且這個概念延伸到其他視覺也很好發揮,只要維持簡單的對比概念就可以了！！
+    .right-side-box
+      img(
+      :src="require('@/assets/images/works_img/griphic/w_m/1.png')"
+    )
 </template>
 
 <script>
@@ -32,6 +40,42 @@ export default {
 
 <style lang="sass">
 
+$right-side-box-width: 30vw
+$right-side-box-img-height: 200px
+$right-side-box-img-position-left: 10vw
+
 #w_m
+  .left-side-box
+    position: fixed
+    top: 0
+    left: 0
+    bottom: 0
+    right: $right-side-box-width + $right-side-box-img-position-left
+    display: flex
+    align-items: center
+    .logo
+      width: 150px
+      position: absolute
+      top: 20px
+      left: 50px
+    h4
+      margin-left: 10px
+      letter-spacing: 3px
+      margin-left: 50px
+      text-align: justify
+  .right-side-box
+    width: $right-side-box-width
+    background-color: #222320
+    position: fixed
+    top: 0
+    bottom: 0
+    right: 0
+    display: flex
+    align-items: center
+    img
+      height: $right-side-box-img-height
+      position: absolute
+      left: -$right-side-box-img-position-left
+
   
 </style>
