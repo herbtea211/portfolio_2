2b2379e1cdcae89f083d84fd569819de31105295
鍾元榮 <herbtea211@gmail.com>
2020-09-01 14:43:22 +0800

    done

--- src/components/works_compoments/gripgic/Coffee_Plus.vue
+++ src/components/works_compoments/gripgic/Coffee_Plus.vue
@@ -3,7 +3,8 @@
     img.poster(
       :src="require('@/assets/images/works_img/griphic/coffee+/0.png')"
     )
-    h4 品書比國起遊起一係麼沒心文有主兩馬給運我性始金？石查其地的高成……足同找些的……朋作國麼年全園無母清來長、麼特這女銷洲告，也對優子上，也所第種岸老每不可育上眼了明父果車。感我一車關法顧目國以快日當的斷是產一。有童而幾金山舉視就老單，自三天小紀的子克國理臉時！呢器幾兩
+    h4 業主是女生,好不容易找到店面在台南可以開個咖啡廳,有別於其他店面經常使用工業風,業主喜歡偏溫馨可愛的手繪風,手繪風格也有分很多種～～看過店面之後示意圖之後,在這裡用的是溫馨感比較強烈的塗鴉方式
+    h4 當然一開始是很搞剛,這裡的物件都是原創的不是素材,每個物件都要一個一個調整,但好處是,一但完成之後會有很多素材可以利用,而且都是照業主想法繪製出的原創素材,雖然準備期很長,但是效果很好
     el-row.name-card-box(:gutter="20")
       el-col(:sm="8")
         img(
@@ -17,7 +18,7 @@
         img(
           :src="require('@/assets/images/works_img/griphic/coffee+/3.png')"
         )
-    h4 品書比國起遊起一係麼沒心文有主兩馬給運我性始金？石查其地的高成……足同找些的……朋作國麼年全園無母清來長、麼特這女銷洲告，也對優子上，也所第種岸老每不可育上眼了明父果車。感我一車關法顧目國以快日當的斷是產一。有童而幾金山舉視就老單，自三天小紀的子克國理臉時！呢器幾兩
+    h4 配色的話也可以有很多種變化,主要就是橘色,咖啡色,下去搭配就好,名片地址的部分也維持可愛的感覺做成一個微笑的樣子,因為製作當時業主店面還沒完工,所以多做一些顏色的搭配,之後裝潢完成可以根據整體風格搭配適合的配色,當然也可以混用
     el-row.catalog-box
       el-col.catalog-img(:sm="12")
         img(
@@ -53,10 +54,13 @@
             img(
               :src="require('@/assets/images/works_img/griphic/coffee+/8.png')"
             )
-    h4 品書比國起遊起一係麼沒心文有主兩馬給運我性始金？石查其地的高成……足同找些的……
-    img.cup(
-      :src="require('@/assets/images/works_img/griphic/coffee+/9.png')"
-    )
+    el-row.cup-box
+      el-col(:sm="12")
+        h4 logo也可以當成連續圖案使用,剛好外帶杯可以用這種做法,有另一種趣味感覺
+      el-col(:sm="12")
+        img.cup(
+          :src="require('@/assets/images/works_img/griphic/coffee+/9.png')"
+        )
 </template>
 
 <script>
@@ -119,6 +123,7 @@ export default {
         width: 80%
         @include imgStyle
     .coaster-box
+      margin-bottom: 50px
       .el-row
         .el-col
           margin-bottom: 20px
@@ -128,8 +133,11 @@ export default {
             -moz-filter: drop-shadow(3px 3px 2px #a5a5a5)
             -o-filter: drop-shadow(3px 3px 2px #a5a5a5)
             filter: drop-shadow(3px 3px 2px #a5a5a5)
-  .cup
-    margin-bottom: 100px
+  .cup-box
+    display: flex
+    align-items: center
+    .cup
+      margin-bottom: 100px
 
 @media screen and (max-width: 767px)
   #coffee-plus
@@ -147,6 +155,8 @@ export default {
         .el-row
           .el-col
             img
+    .cup-box
+      display: unset
 
 
 </style>
