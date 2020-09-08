99d3fa6d367f152cebbb5836a18261c3b46074fc
鍾元榮 <herbtea211@gmail.com>
2020-09-08 16:59:13 +0800

    add img box

--- src/components/works_compoments/ui/Shang_Qing_Go.vue
+++ src/components/works_compoments/ui/Shang_Qing_Go.vue
@@ -1,11 +1,26 @@
 <template lang="pug">
-  #Shang-Qing-Go
-    h1 Shang-Qing-Go      
+  #Shang-Qing-Go.bg-white
+    img.top-img(
+        :src="require('@/assets/images/works_img/ui/shang_qing_go/0.png')"
+      )
+    img(
+        :src="require('@/assets/images/works_img/ui/shang_qing_go/1.png')"
+      )
+    img(
+        :src="require('@/assets/images/works_img/ui/shang_qing_go/2.png')"
+      )
+    img(
+        :src="require('@/assets/images/works_img/ui/shang_qing_go/3.png')"
+      )
+    img(
+        :src="require('@/assets/images/works_img/ui/shang_qing_go/4.png')"
+      )
+    img(
+        :src="require('@/assets/images/works_img/ui/shang_qing_go/5.png')"
+      )
 </template>
 
 <script>
-
-
 
 export default {
   name: 'shangQingGo',
@@ -35,6 +50,13 @@ export default {
 
 @import '../../../assets/styles/var'
 
-#Shang-Qing-Go
+#Shang-Qing-Go.bg-white
+  background-color: #ffffff
+  .top-img
+    width: 80%
+    display: block
+    margin: 0 auto
+    padding-top: 50px
+    padding-bottom: 50px
   
 </style>
